2d35c45819b319adfa0867ba3eb17651e7e87718
zhangjie (CD) <z00484460@china.huawei.com>
2019-09-29 16:06:06 +0800

    base

---
+++ config/defaultSettings.js
@@ -0,0 +1,17 @@
+export default {
+  navTheme: 'dark',
+  primaryColor: '#13C2C2',
+  layout: 'topmenu',
+  contentWidth: 'Fluid',
+  fixedHeader: true,
+  autoHideHeader: false,
+  fixSiderbar: false,
+  menu: {
+    locale: true,
+  },
+  title: 'Ant Design Pro',
+  pwa: false,
+  iconfontUrl: '',
+  collapse: true,
+  language: 'zh-CN',
+};
