65c5d5b0240e826385cf6ede7a5c69aac17601af
zj <zj@zjdeMacBook-Pro.local>
2019-11-20 14:07:02 +0800

    add skill

--- src/pages/skill/form.js
+++ src/pages/skill/form.js
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react';
-import { Input, Form, Select, Upload, Button, Icon, message, Drawer } from 'antd';
+import { Input, Form, Select, Icon, Drawer } from 'antd';
 import { connect } from 'dva';
-import { uploadImgFromPaste } from '@/utils/utils';
 import ReactMarkdown from 'react-markdown';
 import { Controlled as CodeMirror } from 'react-codemirror2';
 
@@ -9,25 +8,11 @@ require('codemirror/lib/codemirror.css');
 require('codemirror/theme/monokai.css');
 require('codemirror/mode/markdown/markdown');
 
-function beforeUpload(file) {
-  const isJPG = (file.type === 'image/jpeg' || file.type === 'image/png' || file.type === 'image/gif');
-  if (!isJPG) {
-    message.error('只能上传图片文件');
-  }
-  const isLt2M = file.size / 1024 / 1024 < 2;
-  if (!isLt2M) {
-    message.error('图片文件不能大于2MB!');
-  }
-  return isJPG && isLt2M;
-}
-
 @connect()
 @Form.create()
 class BasicForm extends PureComponent {
   state = {
     source: '',
-    loading: false,
-    fileList: [],
     viewVisible: false,
   }
 
@@ -37,13 +22,6 @@ class BasicForm extends PureComponent {
     if (updateData) {
       this.setState({
         source: updateData.content ? unescape(updateData.content) : '',
-        fileList: updateData.imagename ? [{
-          uid: -1,
-          name: updateData.imagename,
-          status: 'done',
-          response: { imagename: updateData.imagename },
-          url: `/upload/${updateData.imagename}`,
-        }] : [],
       });
     }
   }
@@ -52,41 +30,12 @@ class BasicForm extends PureComponent {
   componentDidMount() {
   }
 
-  handleChange = info => {
-    let { fileList } = info;
-    fileList = fileList.slice(-1);
-    fileList = fileList.map(file => {
-      const tmp = file;
-      if (tmp.response) {
-        tmp.url = `/upload/${file.response.imagename}`;
-      }
-      return tmp;
-    });
-    this.setState({ fileList });
-  }
-
-  removeImg = info => {
-    const { dispatch } = this.props;
-    dispatch({
-      type: 'global/deleteImg',
-      id: info.response.imagename,
-    })
-  }
-
   changeCodeMirror = (editor, data, value) => {
     const { form } = this.props;
     this.setState({
       source: value,
     });
     form.setFieldsValue({ content: value });
-  }
-
-  normFile = e => {
-    if (Array.isArray(e)) {
-      return e;
-    }
-    // eslint-disable-next-line max-len
-    return (e && e.fileList && e.fileList[0] && e.fileList[0].response && e.fileList[0].response.imagename) || '';
   }
 
   showSource = () => {
@@ -101,29 +50,11 @@ class BasicForm extends PureComponent {
     });
   };
 
-  onPaste =(editor, e) => {
-    if (!(e.clipboardData && e.clipboardData.items)) {
-      return;
-    }
-    for (let i = 0, len = e.clipboardData.items.length; i < len; i += 1) {
-      const item = e.clipboardData.items[i];
-      if (item.kind === 'file') {
-        const pasteFile = item.getAsFile();
-        // pasteFile就是获取到的文件
-        this.editor = editor;
-        uploadImgFromPaste(pasteFile, '/api/upload/paste', res => {
-          // 设置上传图片粘贴值
-          editor.replaceSelection(`![](/upload/${res.imagename})`);
-        });
-      }
-    }
-  }
-
   // jsx渲染
   render() {
     // 页面内容
     const { updateData, form: { getFieldDecorator } } = this.props;
-    const { source, fileList, loading, viewVisible } = this.state;
+    const { source, viewVisible } = this.state;
     const formItemLayout = {
       labelCol: {
         xs: { span: 24 },
@@ -154,14 +85,14 @@ class BasicForm extends PureComponent {
         </Form.Item>
         <Form.Item {...formItemLayout} label="作者">
           {getFieldDecorator('author', {
-            initialValue: updateData ? updateData && updateData.title : '16to',
+            initialValue: updateData ? updateData && updateData.author : '16to',
             rules: [
               {
                 required: true,
                 message: '必填',
               },
             ],
-          })(<Input placeholder="请输入标题" />)}
+          })(<Input placeholder="请输入作者" />)}
         </Form.Item>
         <Form.Item {...formItemLayout} label="类型">
           {getFieldDecorator('type', {
@@ -179,26 +110,16 @@ class BasicForm extends PureComponent {
             </Select>,
           )}
         </Form.Item>
-        <Form.Item {...formItemLayout} label="单个图片">
-          {getFieldDecorator('imagename', {
-            getValueFromEvent: this.normFile,
-            initialValue: updateData ? updateData && updateData.imagename : '',
-          })(
-            <Upload
-              onChange={this.handleChange}
-              listType="picture"
-              action="/api/upload"
-              beforeUpload={beforeUpload}
-              fileList={fileList}
-              name="bgimg"
-              onRemove={this.removeImg}
-            >
-              {fileList && fileList.length === 0 ? (<Button type="primary">
-                <Icon type="upload" /> 上传{loading}
-              </Button>) : null
-              }
-            </Upload>,
-          )}
+        <Form.Item {...formItemLayout} label="排序">
+          {getFieldDecorator('sort', {
+            initialValue: updateData ? updateData && updateData.sort : 0,
+            rules: [
+              {
+                required: true,
+                message: '必填',
+              },
+            ],
+          })(<Input placeholder="请输入排序，数字越大越靠前" />)}
         </Form.Item>
         <Form.Item {...formItemLayout} label={preView}>
           {getFieldDecorator('content', {
