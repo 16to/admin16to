7a7abf915aef6ef4d1c202bf8fddf19679bbf3c9
zj <zj@zjdeMacBook-Pro.local>
2019-11-28 09:53:53 +0800

    fix bug

--- config/router.config.js
+++ config/router.config.js
@@ -29,40 +29,40 @@ export default [
             icon: "home",
             component: "./Home"
           },
-          // 表格功能
-          {
-            path: "/table",
-            name: "table",
-            icon: "smile",
-            component: "../layouts/ContentLayout",
-            routes: [
-              {
-                path: "/table",
-                hideInMenu: true,
-                component: "./table/index"
-              },
-              {
-                path: "/table/add",
-                name: "add",
-                hideInMenu: true,
-                component: "./table/add"
-              },
-              {
-                path: "/table/update/:id",
-                name: "update",
-                hideInMenu: true,
-                component: "./table/update"
-              },
-              {
-                component: "./404"
-              }
-            ]
-          },
-          // 表格功能
+          // // 表格功能
+          // {
+          //   path: "/table",
+          //   name: "table",
+          //   icon: "smile",
+          //   component: "../layouts/ContentLayout",
+          //   routes: [
+          //     {
+          //       path: "/table",
+          //       hideInMenu: true,
+          //       component: "./table/index"
+          //     },
+          //     {
+          //       path: "/table/add",
+          //       name: "add",
+          //       hideInMenu: true,
+          //       component: "./table/add"
+          //     },
+          //     {
+          //       path: "/table/update/:id",
+          //       name: "update",
+          //       hideInMenu: true,
+          //       component: "./table/update"
+          //     },
+          //     {
+          //       component: "./404"
+          //     }
+          //   ]
+          // },
+          // 技术积累功能
           {
             path: "/skill",
             name: "skill",
-            icon: "file-text",
+            icon: "file-markdown",
             component: "../layouts/ContentLayout",
             routes: [
               {
@@ -81,6 +81,35 @@ export default [
                 name: "update",
                 hideInMenu: true,
                 component: "./skill/update"
+              },
+              {
+                component: "./404"
+              }
+            ]
+          },
+          // 热点专题功能
+          {
+            path: "/special",
+            name: "special",
+            icon: "fire",
+            component: "../layouts/ContentLayout",
+            routes: [
+              {
+                path: "/special",
+                hideInMenu: true,
+                component: "./special/index"
+              },
+              {
+                path: "/special/add",
+                name: "add",
+                hideInMenu: true,
+                component: "./special/add"
+              },
+              {
+                path: "/special/update/:id",
+                name: "update",
+                hideInMenu: true,
+                component: "./special/update"
               },
               {
                 component: "./404"
